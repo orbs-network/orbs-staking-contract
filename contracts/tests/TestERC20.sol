80e9462e2a556980bf8573e8787a896c5f329096
Leonid Beder <leonid.beder@gmail.com>
2019-09-03 19:47:41 +0300

    Add TestERC20.sol

---
+++ contracts/tests/TestERC20.sol
@@ -0,0 +1,35 @@
+pragma solidity 0.4.26;
+
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+
+
+contract TestERC20 is ERC20 {
+    bool failApprove;
+    bool failTransfer;
+
+    function setFailure(bool _failApprove, bool _failTransfer) public {
+        failApprove = _failApprove;
+        failTransfer = _failTransfer;
+    }
+
+    function assign(address _account, uint256 _value) public {
+        _burn(_account, balanceOf(_account));
+        _mint(_account, _value);
+    }
+
+    function approve(address spender, uint256 value) public returns (bool) {
+        if (failApprove) {
+            return false;
+        }
+
+        return super.approve(spender, value);
+    }
+
+    function transfer(address to, uint256 value) public returns (bool) {
+        if (failTransfer) {
+            return false;
+        }
+
+        return super.transfer(to, value);
+    }
+}
