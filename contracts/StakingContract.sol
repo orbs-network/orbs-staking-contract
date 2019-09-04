6114a30e0e38fa6d6d06230494ded3f50e4d22f1
Leonid Beder <leonid.beder@gmail.com>
2019-09-04 19:25:06 +0300

    Add StakingContract

---
+++ contracts/StakingContract.sol
@@ -0,0 +1,9 @@
+pragma solidity 0.4.26;
+
+import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+
+
+// @title Orbs staking smart contract.
+contract StakingContract {
+}
