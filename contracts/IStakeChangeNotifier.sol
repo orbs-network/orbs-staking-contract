dd32394d824654503f344b441971854de40fa9ca
Leonid Beder <leonid.beder@gmail.com>
2019-09-05 12:05:57 +0300

    Add doc

--- contracts/IStakeChangeNotifier.sol
+++ contracts/IStakeChangeNotifier.sol
@@ -3,5 +3,7 @@ pragma solidity 0.4.26;
 
 /// @title An interface for notifying of stake change events (e.g., stake, unstake, partial unstake, restate, etc.).
 interface IStakeChangeNotifier {
-    function stakeChange(address _staker) external;
+    /// @dev Notifies of stake change event.
+    /// @param _stakeOwner address The address of the subject stake owner.
+    function stakeChange(address _stakeOwner) external;
 }
