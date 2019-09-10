86cc8abd06cd40b6697ad2b165d466a06988d91b
Leonid Beder <leonid.beder@gmail.com>
2019-09-10 14:46:33 +0300

    Add getToken to IStakingContract

--- contracts/IStakingContract.sol
+++ contracts/IStakingContract.sol
@@ -1,7 +1,12 @@
 pragma solidity 0.4.26;
 
+import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
+
 /// @title An interface for staking contracts.
 interface IStakingContract {
+    /// @dev Returns the address of the underlying staked token.
+    function getToken() view external returns (IERC20);
+
     /// @dev Stakes ORBS tokens on behalf of msg.sender.
     /// @param _amount uint256 The number of tokens to stake.
     ///
