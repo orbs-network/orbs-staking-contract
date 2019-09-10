524fe1c0cc65710417913791545dc67824f73e4c
Leonid Beder <leonid.beder@gmail.com>
2019-09-10 14:46:49 +0300

    Remove unnecessary import

--- contracts/StakingContract.sol
+++ contracts/StakingContract.sol
@@ -1,6 +1,5 @@
 pragma solidity 0.4.26;
 
-import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 import "./IStakingContract.sol";
