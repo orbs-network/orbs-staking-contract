0bf1829eb37578fc42e4c02a71266fecdf76fb54
Leonid Beder <leonid.beder@gmail.com>
2019-09-20 18:28:12 +0300

    Move notifier test contracts to a directory of their own

--- contracts/tests/notifiers/ReentrantStakeChangeNotifierMock.sol
+++ contracts/tests/notifiers/ReentrantStakeChangeNotifierMock.sol
@@ -2,7 +2,7 @@ pragma solidity 0.4.26;
 
 import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
 
-import "../IStakingContract.sol";
+import "../../IStakingContract.sol";
 import "./StakeChangeNotifierMock.sol";
 
 /// @title A reentrancy test mockup for IStakeChangeNotifier.
--- contracts/tests/notifiers/StakeChangeNotifierMock.sol
+++ contracts/tests/notifiers/StakeChangeNotifierMock.sol
@@ -1,6 +1,6 @@
 pragma solidity 0.4.26;
 
-import "../IStakeChangeNotifier.sol";
+import "../../IStakeChangeNotifier.sol";
 
 /// @title A test mockup for IStakeChangeNotifier.
 contract StakeChangeNotifierMock is IStakeChangeNotifier {
