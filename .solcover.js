b8ec5ec2ff7d1416936703db3761c550dd3a4f90
Leonid Beder <leonid.beder@gmail.com>
2020-01-22 20:36:15 +0200

    Switch to internal ganache-core and update solidity-coverage to 0.7.0

--- .solcover.js
+++ .solcover.js
@@ -1,9 +1,13 @@
 module.exports = {
-  testCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle test --network coverage',
-  compileCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle compile --network coverage',
+  testCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle run coverage',
+  compileCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle compile',
   copyPackages: ['@openzeppelin/contracts'],
   norpc: true,
   skipFiles: [
     'tests'
-  ]
+  ],
+  providerOptions: {
+    total_accounts: 30,
+    defaultEtherBalance: 1000
+  }
 };
