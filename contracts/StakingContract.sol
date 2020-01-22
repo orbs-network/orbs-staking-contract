bb683294045e554e80efd5c6692fe2817a6e0531
Leonid Beder <leonid.beder@gmail.com>
2020-01-22 18:47:14 +0200

    Update openzeppelin-solidity to @openzeppelin/contracts v2.3.0 (without GSN)

--- contracts/StakingContract.sol
+++ contracts/StakingContract.sol
@@ -1,6 +1,6 @@
 pragma solidity 0.4.26;
 
-import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
 
 import "./IStakingContract.sol";
 import "./IStakeChangeNotifier.sol";
--- contracts/tests/notifiers/ReentrantStakeChangeNotifierMock.sol
+++ contracts/tests/notifiers/ReentrantStakeChangeNotifierMock.sol
@@ -1,6 +1,6 @@
 pragma solidity 0.4.26;
 
-import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 import "../../IStakingContract.sol";
 import "./StakeChangeNotifierMock.sol";
