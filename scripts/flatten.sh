182699fd8607694b6dfbdfc132d188687b9c8f0e
OdedWx <odedw@orbs.com>
2020-03-30 17:34:37 +0300

    flatten script and header

--- scripts/flatten.sh
+++ scripts/flatten.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+# header (comments)
 cp ./header.txt ./StakingContract_flattened.sol	
+
+# append the flattened contracts after the header
 ../node_modules/.bin/truffle-flattener ../contracts/StakingContract.sol >> ./StakingContract_flattened.sol
